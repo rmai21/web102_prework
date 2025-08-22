82d3ab320de8602adad637dc7939e2e37a921948
Rajvi Maisuria <50464351+rmai21@users.noreply.github.com>
2025-08-22 16:55:38 -0400

    Challenge 4 completed

--- index.js
+++ index.js
@@ -67,20 +67,20 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce((acc, game) => acc + game.backers, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${totalContributions.toLocaleString()}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+const totalRaised = GAMES_JSON.reduce((acc, game) => acc + game.pledged, 0)
 
 // set inner HTML using template literal
-
-
+raisedCard.innerHTML = totalRaised.toLocaleString("en-US", { style: "currency", currency: "USD" });
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
-
+gamesCard.innerHTML = GAMES_JSON.length.toLocaleString();
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
